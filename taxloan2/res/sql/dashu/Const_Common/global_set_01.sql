9bd5951a88df772344873cd8b123cceffb75a552
acewind1 <36976754+acewind1@users.noreply.github.com>
2019-11-28 23:02:21 +0800

    191126 - SQL 常量里面修改 Hive 库到 dm_taxloan1

--- taxloan2/res/sql/dashu/Const_Common/global_set_01.sql
+++ taxloan2/res/sql/dashu/Const_Common/global_set_01.sql
@@ -1,6 +1,6 @@
 
-set hivevar:DATABASE_SRC=dm_taxloan2;
-set hivevar:DATABASE_DEST=dm_taxloan2;
+set hivevar:DATABASE_SRC=dm_taxloan1;
+set hivevar:DATABASE_DEST=dm_taxloan1;
 set hive.exec.dynamic.partition=true;
 set hive.exec.dynamic.partition.mode=nonstrict;
 use ${hivevar:DATABASE_DEST}
--- taxloan2/res/sql/taxloan1/Const_Common/global_set_01.sql
+++ taxloan2/res/sql/taxloan1/Const_Common/global_set_01.sql
@@ -4,7 +4,7 @@ set hivevar:DATABASE_DEST=dm_taxloan2   ;
 set hivevar:DATABASE_DEST=dm_taxloan2333 ----- sdasdsad ---ds sdadsasd
 set hivevar:DATABASE_DEST=dm_taxloan233; ----- sdasdsad ---ds sdadsasd
        -- test
-set hivevar:DATABASE_SRC=dm_taxloan2;
-set hivevar:DATABASE_DEST=dm_taxloan2;
+set hivevar:DATABASE_SRC=dm_taxloan1;
+set hivevar:DATABASE_DEST=dm_taxloan1;
 set hive.exec.dynamic.partition=true;
 set hive.exec.dynamic.partition.mode=nonstrict;
